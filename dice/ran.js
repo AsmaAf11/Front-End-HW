a092f2384fbdf219f2119e0553311c07b2480f76
AsmaAf11 <79783546+AsmaAf11@users.noreply.github.com>
2022-06-21 18:14:55 +0300

    Add files via upload

---
+++ dice/ran.js
@@ -0,0 +1,25 @@
+function numberSum(N) {
+    var total = 0;
+      for(var i = 1; i <= N; i++){
+        total += i;
+      }
+      return total;
+  }
+
+
+console.log(numberSum(55))
+
+
+let numStr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const sumEvens = (numStr) => {
+  let sum = 0;
+  for (let i = 0; i < numStr.length; i++) {
+    if (i % 2 === 0){ 
+    sum = sum + numStr[i];
+    }
+  }
+  return sum;
+}
+ 
+console.log(sumEvens(numStr));
